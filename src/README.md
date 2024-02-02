279a0621fe8c56a0afd69f283db93fb971410309
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2024-02-02 14:14:31 +0100

    added files

---
+++ src/README.md
@@ -0,0 +1,13 @@
+# Fancy-Login-Form
+
+Rebuilt in scala for better integration with Course-Description-Automation project.
+
+**NB**: This project is simply the GUI for the [Course-Description-Automation](https://github.com/David-Kyrat/Course-Description-Automation) project.
+It was before "hidden" in a jar in the resource directory that was "manually" launched with shell command.  
+However:
+ - this is obviously not best practice, as it is hard to maintain (because not modifiable).
+ - when building  [Course-Description-Automation](https://github.com/David-Kyrat/Course-Description-Automation) from source, the gui needs to be packaged into a jar appart from the main project and moved to the resource directory. (Even though building from source is not required to run project, it is still nice to have.)
+
+The configuration files *fli_scala.iml, .idea/* ... are left in this repository on purpose, to simplify further tinkering
+of build configuration with IntelliJ.
+The end project doesn't require Intellij, or any specific configuration file to run (aside of the Makefile of course).
---
+++ src/cda/App.scala
@@ -0,0 +1,41 @@
+package cda
+
+import cda.model.Main as modelMain
+import cda.model.Utils.{abbrevFilename, pathOf}
+import cda.model.pandoc.PandocCommand.toVec
+import cda.model.pandoc.Timer.{time_and_result, time_average}
+import cda.model.pandoc.{Colorify, PandocCommand}
+import cda.view.jfxuserform
+import javafx.application.{Application, Platform}
+
+import java.nio.file.Path
+
+// j.Main.main(args) launches gui
+object App {
+    var verbose = false
+    val abbrevFilePath: Path = pathOf(abbrevFilename)
+    val absAbbrevFilePath: String = abbrevFilePath.normalize().toAbsolutePath.toString
+
+    def main(args: Array[String]): Unit = {
+        try
+            verbose = args.contains("verbose")
+            val guiMain = new jfxuserform.Main()
+            // launch gui and blocks until gui is closed
+            guiMain.start(Array(absAbbrevFilePath))
+            val userResponse = guiMain.serializedOutput.get
+            // FIXME: Even when setting PandocCommand.exec(silent=false)
+            //    stderr is not printed to console. i.e. nothing tells us whether pandoc failed, and if it did => no way of knowing why
+            // val userResponse = "11M020,11M010,12m061,11X001#"
+            modelMain.main(Array(userResponse))
+        catch
+            case e: Throwable => e.printStackTrace()
+        finally {
+            onExit()
+        }
+    }
+
+    /**
+     * See `ch.view.jfxuserform.Main.silenceBurningWaveLogsAfterStageClose()` for more details.
+     */
+    private def onExit(): Unit = jfxuserform.Main.silenceBurningWaveLogsAfterStageClose()
+}
